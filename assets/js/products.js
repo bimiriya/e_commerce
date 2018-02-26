0fe149a704791172880da3d79176c2c6d20a6a54
Niennis <estefania.osses.v@gmail>
2018-02-26 17:59:01 -0300

    Views maqueteadas con api mercado libre

---
+++ assets/js/products.js
@@ -0,0 +1,161 @@
+var quoteInfo = document.getElementById('quote-template').innerHTML;
+var template = Handlebars.compile(quoteInfo);
+
+
+// ---------------------------- BOTONES PARA CARGAR PRODUCTOS SEGUN CATEGORIA
+var vintageBtn = document.getElementById('vintageBtn');
+vintageBtn.addEventListener('click', showVintage);
+
+var homeDecoBtn = document.getElementById('homeDecoBtn');
+homeDecoBtn.addEventListener('click', showDeco);
+
+var artBtn = document.getElementById('artBtn');
+artBtn.addEventListener('click', showArt);
+
+var techBtn = document.getElementById('techBtn');
+techBtn.addEventListener('click', showTech);
+
+var booksBtn = document.getElementById('booksBtn');
+booksBtn.addEventListener('click', showBooks);
+
+var adultsBtn = document.getElementById('adultsBtn');
+adultsBtn.addEventListener('click', showAdults);
+
+// ---------------------------- FUNCIONES QUE CARGAN CONTENIDO
+function showVintage() {
+  document.getElementById('app').innerHTML = '';
+  var data = fetch('https://api.mercadolibre.com/sites/MLC/search?q=vintage')
+    .then(response => response.json())
+  
+    .then(data => {
+      let quoteData;
+      for (let i = 0; i < data.results.length; i++) {
+        quoteData = template({
+          decoHome: [
+            {decoHome: data.results[i].title}],
+          thumb: [
+            {thumbnail: data.results[i].thumbnail}],
+          available: [
+            {available: data.results[i].available_quantity}],
+          price: [
+            {price: data.results[i].price}]          
+        });
+        document.getElementById('app').innerHTML += quoteData;
+      };
+    });
+}
+
+function showDeco() {
+  document.getElementById('app').innerHTML = '';
+  var data = fetch('https://api.mercadolibre.com/sites/MLC/search?q=hogar')
+    .then(response => response.json())
+  
+    .then(data => {
+      let quoteData;
+      for (let i = 0; i < data.results.length; i++) {
+        quoteData = template({
+          decoHome: [
+            {decoHome: data.results[i].title}],
+          thumb: [
+            {thumbnail: data.results[i].thumbnail}],
+          available: [
+            {available: data.results[i].available_quantity}],
+          price: [
+            {price: data.results[i].price}]          
+        });
+        document.getElementById('app').innerHTML += quoteData;
+      };
+    });
+}
+
+function showArt() {
+  document.getElementById('app').innerHTML = '';
+  var data = fetch('https://api.mercadolibre.com/sites/MLC/search?q=arte')
+    .then(response => response.json())
+  
+    .then(data => {
+      let quoteData;
+      for (let i = 0; i < data.results.length; i++) {
+        quoteData = template({
+          decoHome: [
+            {decoHome: data.results[i].title}],
+          thumb: [
+            {thumbnail: data.results[i].thumbnail}],
+          available: [
+            {available: data.results[i].available_quantity}],
+          price: [
+            {price: data.results[i].price}]          
+        });
+        document.getElementById('app').innerHTML += quoteData;
+      };
+    });
+}
+
+function showTech() {
+  document.getElementById('app').innerHTML = '';
+  var data = fetch('https://api.mercadolibre.com/sites/MLC/search?q=tecnologia')
+    .then(response => response.json())
+  
+    .then(data => {
+      let quoteData;
+      for (let i = 0; i < data.results.length; i++) {
+        quoteData = template({
+          decoHome: [
+            {decoHome: data.results[i].title}],
+          thumb: [
+            {thumbnail: data.results[i].thumbnail}],
+          available: [
+            {available: data.results[i].available_quantity}],
+          price: [
+            {price: data.results[i].price}]          
+        });
+        document.getElementById('app').innerHTML += quoteData;
+      };
+    });
+}
+
+function showBooks() {
+  document.getElementById('app').innerHTML = '';
+  var data = fetch('https://api.mercadolibre.com/sites/MLC/search?q=books')
+    .then(response => response.json())
+  
+    .then(data => {
+      let quoteData;
+      for (let i = 0; i < data.results.length; i++) {
+        quoteData = template({
+          decoHome: [
+            {decoHome: data.results[i].title}],
+          thumb: [
+            {thumbnail: data.results[i].thumbnail}],
+          available: [
+            {available: data.results[i].available_quantity}],
+          price: [
+            {price: data.results[i].price}]          
+        });
+        document.getElementById('app').innerHTML += quoteData;
+      };
+    });
+}
+
+function showAdults() {
+  document.getElementById('app').innerHTML = '';
+  var data = fetch('https://api.mercadolibre.com/sites/MLC/search?q=31%20minutos')
+    .then(response => response.json())
+  
+    .then(data => {
+      let quoteData;
+      for (let i = 0; i < data.results.length; i++) {
+        quoteData = template({
+          decoHome: [
+            {decoHome: data.results[i].title}],
+          thumb: [
+            {thumbnail: data.results[i].thumbnail}],
+          available: [
+            {available: data.results[i].available_quantity}],
+          price: [
+            {price: data.results[i].price}]          
+        });
+        document.getElementById('app').innerHTML += quoteData;
+      };
+    });
+}
