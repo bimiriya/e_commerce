9c03ce4b585a76384245be0ce96d2fadfa79fa3f
bimiriya <poyarzunaspee@gmail.com>
2018-02-27 10:10:22 -0300

    comentarios productinfo.js

--- assets/js/productinfo.js
+++ assets/js/productinfo.js
@@ -1,7 +1,12 @@
 const accessToken = 'APP_USR-8189733489067517-022707-815285de0a64dbe771d33f0539e5cf43__J_H__-239442426';
+
+// añadir evento a elemento creado mediante dom + template
 document.addEventListener('click', function(e) {
   if (e.target.id === 'view_product') {
+    // pasar el atributo data id que contiene el id del producto seleccionado para ingresarlo
+    // como campo dentro del fetch
     let dataID = $(e.target).data('id');
+    // función para llamar el objeto del producto seleccionado
     getProductInfo(dataID);
   }
 });
@@ -12,17 +17,24 @@ function getProductInfo(productID) {
       return response.json();
     })
     .then((data) => {
+      // función para rellenar la template
       fillTemplate(data);
+      // se abre el modal
       $('#productModal').modal();
     });
 }
 
 function fillTemplate(data) {
+  // se guarda el contenido del template dentro de una variable
   let rawTemplate = document.getElementById('product-template').innerHTML;
+  // dentro de otra variable se compila el contenido 
   let compiledTemplate = Handlebars.compile(rawTemplate);
+  // la variable donde se compiló el contenido se usa como función y se le pasa la data
+  // obtenida del fecth como argumento
   let generatedHTML = compiledTemplate(data);
 
-  const app = document.getElementById('here');
-  app.innerHTML = generatedHTML;
-  console.log(data);
+  // se recupera el contenedor del html
+  const modal = document.getElementById('here');
+  // y se inserta la template
+  modal.innerHTML = generatedHTML;
 }
