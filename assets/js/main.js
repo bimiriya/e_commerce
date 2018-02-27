e426de54f164bf0e755cd1c69414fb1268abfce9
bimiriya <poyarzunaspee@gmail.com>
2018-02-27 10:51:47 -0300

    testing bootstrap link/script

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,11 @@
 // Mercadopago.createToken(form, tokenHandler);
 
 // ----------------------------- INICIO ROUTING
-
 (function() {
   function init() {
     let router = new Router([
+      new Route('deco', 'deco.html', true),
+      new Route('art', 'art.html'),
       new Route('deco', 'deco.html', true)
     ]);
   }
