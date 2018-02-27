65fcdb5fe1558fb055d211427fac1d622faa64ba
ValeOlivares <valentinaolivares85@gmail.com>
2018-02-26 21:32:35 -0300

    Agregando boton de paypal

--- assets/js/products.js
+++ assets/js/products.js
@@ -29,6 +29,7 @@ function showVintage() {
   
     .then(data => {
       let quoteData;
+      
       for (let i = 0; i < data.results.length; i++) {
         quoteData = template({
           decoHome: [
@@ -43,6 +44,15 @@ function showVintage() {
             {id: data.results[i].id}] 
         });
         document.getElementById('app').innerHTML += quoteData;
+        $('#app').append(
+          `<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="holi">
+            <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>`
+
+          ) 
       };
     });
 }
